35e2868ec1a174765232e370e201629d8a0d5087
Jake Donham <jaked@github.com>
2022-08-30 14:37:37 -0700

    update

--- tools/test-setup-after-env.js
+++
@@ -1,4 +0,0 @@
-import '@testing-library/jest-dom'
-
-// eslint-disable-next-line no-undef
-jest.mock('scheduler', () => require('scheduler/unstable_mock')) // Scheduler needs to be mocked for React 18.x

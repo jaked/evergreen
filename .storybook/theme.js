c68fe0159023fd15ca9407e87052a54d77031568
Jake Donham <jaked@github.com>
2022-08-30 15:11:05 -0700

    update

--- .storybook/theme.js
+++
@@ -1,7 +0,0 @@
-import { create } from '@storybook/theming/create'
-
-export default create({
-  base: 'light',
-  brandTitle: '🌲 Evergreen',
-  brandUrl: 'https://github.com/segmentio/evergreen'
-})

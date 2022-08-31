4b551d2d34bcf8274eb6c44a65d4d33e3b1a2248
Jake Donham <jaked@github.com>
2022-08-31 10:53:28 -0700

    update

--- docs/hooks/useIsomorphicLayoutEffect.ts
+++
@@ -1,5 +0,0 @@
-import { useLayoutEffect, useEffect } from 'react'
-
-const useIsomorphicLayoutEffect = typeof document !== 'undefined' ? useLayoutEffect : useEffect
-
-export default useIsomorphicLayoutEffect
--- docs/types/@segment/consent-manager.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '@segment/consent-manager' {
-  export * from '@segment/consent-manager/types/index'
-}
